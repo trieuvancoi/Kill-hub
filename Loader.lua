5627493b6e653f6cdbeaf8f49c637b74c687ff8a
trieuvancoi <ccoilopc@gmail.com>
2025-09-08 12:03:54 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,18 +1,12 @@
--- Loader.lua
-local gui = Instance.new("ScreenGui", game.CoreGui)
-local txt = Instance.new("TextLabel", gui)
-txt.Size = UDim2.new(1,0,1,0)
-txt.BackgroundTransparency = 1
-txt.Text = "Loading..."
-txt.TextColor3 = Color3.new(1,1,1)
-txt.Font = Enum.Font.SourceSansBold
-txt.TextSize = 40
+-- Redz Kill Aura | Loader
+local url = "https://raw.githubusercontent.com/trieuvancoi/Redz-kill/main/Main.lua"
 
-task.wait(2)
-txt.Text = "Almost Ready..."
+local success, response = pcall(function()
+    return game:HttpGet(url)
+end)
 
-task.wait(1)
-gui:Destroy()
-
--- Gọi Main.lua từ GitHub
-loadstring(game:HttpGet("https://raw.githubusercontent.com/trieuvancoi/Kill-hub/main/Main.lua"))()
+if success then
+    loadstring(response)()
+else
+    warn("[Redz Kill Aura] Không thể tải script từ GitHub")
+end
